ac5732ed7711f771c709898129672082d54b590e
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-08-09 14:46:31 +0900

    2021-08-09-1500

--- servlet/EditorServlet.java
+++ servlet/EditorServlet.java
@@ -11,17 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/EditorServlet")
 public class EditorServlet extends MainVenderBundleServlet {
     private static final long serialVersionUID = 1L;
-    private String edit;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.edit = (String) request.getParameter("edit");
-        String path = "";
-        if(edit.equals("append")) {
-            path = "/WEB-INF/view/append.jsp";
-        } else if(edit.equals("delete")) {
-            path = "/WEB-INF/view/delete.jsp";
-        }
-
+        String path = "/WEB-INF/view/venderEditor.jsp";
         RequestDispatcher dis = request.getRequestDispatcher(path);
         dis.forward(request, response);
     }//doGet()
