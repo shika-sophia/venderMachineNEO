62b7af32d7f7c152ef2b3d9976ed992e434183bc
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-08-10 11:52:20 +0900

    2021-08-10-1200

--- servlet/EditorServlet.java
+++ servlet/EditorServlet.java
@@ -1,3 +1,26 @@
+/**
+ * @title venderMachineNEO/servlet/EditorServlet.java
+ * @file /WEB-INF/view/venderEditor.jsp
+ * @formAction EditorServlet
+ * @formMethod POST
+ * @content drinkList, priceListの編集・追加・削除の入力フォーム
+ *
+ * @inputData ---- jsp -> Servlet ----
+ * @param id -> indexEditList
+ * @param dr -> drinkEditList
+ * @param pr -> priceEditList
+ * @param ap -> appendEditList
+ * @param de -> deleteEditList
+ *
+ * @requestQuery http://localhost:8080/venderMachineNEO/EditorServlet
+ *   ?id=&id=&id=&id=&id=&ap=50
+ *   &dr=&dr=&dr=&dr=&dr=Milk&ap=Soda&ap=SodaEn
+ *   &pr=&pr=&pr=&pr=&pr=&ap=1.20&de=de3
+ *
+ * @author shika
+ * @date 2021-08-10
+ */
+
 package servlet;
 
 import java.io.IOException;
@@ -7,6 +30,9 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.EditData;
+import model.EditLogic;
 
 @WebServlet("/EditorServlet")
 public class EditorServlet extends MainVenderBundleServlet {
@@ -19,9 +45,17 @@ public class EditorServlet extends MainVenderBundleServlet {
     }//doGet()
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //String[] editAry = request.getParameterValues("editAry");
-        //parse.parseEdit(edit, editAry);
+        String[] indexEditAry = request.getParameterValues("id");
+        String[] drinkEditAry = request.getParameterValues("dr");
+        String[] priceEditAry = request.getParameterValues("pr");
+        String[] appendEditAry = request.getParameterValues("ap");
+        String[] deleteEditAry = request.getParameterValues("de");
+        EditData editData = new EditData(
+                indexEditAry, drinkEditAry, priceEditAry,
+                appendEditAry, deleteEditAry);
 
+        EditLogic editLogic = new EditLogic(editData);
+        editLogic.editParse();
         super.doGet(request, response);
     }//doPost()
 
