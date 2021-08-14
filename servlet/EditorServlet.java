02bf6c956d7a1d9b14963ad779fc038d03e4f0e2
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-08-14 13:12:27 +0900

    2021-08-14-1300

--- servlet/EditorServlet.java
+++ servlet/EditorServlet.java
@@ -30,23 +30,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.EditData;
 import model.EditInputSecurity;
+import model.EditMessage;
+import model.EditTempLogic;
 
 @WebServlet("/EditorServlet")
 public class EditorServlet extends MainVenderBundleServlet {
     private static final long serialVersionUID = 1L;
-    private EditData editData;
-    private EditInputSecurity security;
+    protected EditData editData;
+    protected EditInputSecurity security;
+    protected EditTempLogic editTemp;
+    protected EditMessage editMess;
 
     public void init(ServletConfig config)
             throws ServletException {
         editData = new EditData();
         security = new EditInputSecurity(editData);
+        editTemp = new EditTempLogic(editData);
+        editMess = new EditMessage(locale);
     }//init()
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = "/WEB-INF/view/venderEditor.jsp";
-        RequestDispatcher dis = request.getRequestDispatcher(path);
-        dis.forward(request, response);
+        doForward(request, response, path);
     }//doGet()
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -59,12 +64,34 @@ public class EditorServlet extends MainVenderBundleServlet {
             indexEditAry, drinkEditAry, priceEditAry,
             appendEditAry, deleteEditAry);
 
+        //入力チェック(スクリプトタグ)
         security.setEditList();
         boolean canAccept = security.checkListElement();
+        editMess.acceptMsg(canAccept);
+        if(!canAccept) {
+            String path = "/WEB-INF/view/venderEditor.jsp";
+            doForward(request, response, path);
+        }
 
-        String path = "/WEB-INF/veiw/editConfirm.jsp";
+        //入力チェック(appendEditList)
+        boolean canAppend = editTemp.appendOperation(editMess);
+        if(!canAppend) {
+            String path = "/WEB-INF/view/venderEditor.jsp";
+            doForward(request, response, path);
+        }
+
+        editTemp.sortByIndex();
+
+        String path = "/WEB-INF/veiw/venderEditConfirm.jsp";
+        doForward(request, response, path);
+    }//doPost()
+
+    private void doForward(
+            HttpServletRequest request, HttpServletResponse response,
+            String path) throws ServletException, IOException {
+
         RequestDispatcher dis = request.getRequestDispatcher(path);
         dis.forward(request, response);
-    }//doPost()
+    }//doForward()
 
 }//class
