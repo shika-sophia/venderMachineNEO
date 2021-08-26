4f0dc2329d144eae57a18bb1be015041c1cbf401
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-08-26 12:54:06 +0900

    2021-08-26-1300

--- model/EditSortIndex.java
+++ model/EditSortIndex.java
@@ -1,3 +1,14 @@
+/**
+ * ◆ソートのアルゴリズム
+ * 元の入力インデックスを保存しておき、0～50
+ * それを数字昇順にソート。
+ * 例) 0, 25, 20, 30, 40 //indexEditList
+ * 例) 0, 20, 25, 30, 40 //sortedIndexList
+ * ソート済の並び順は、元のListの何番目indexかを求め、
+ * List.indexを取り出し 0～5
+ * 例) 0, 2, 1, 3, 4     //indexOriginList
+ * 各Listを List.index順に addしていく。
+ */
 package model;
 
 import java.util.ArrayList;
@@ -6,24 +17,37 @@ import java.util.List;
 public class EditSortIndex extends EditTempLogic {
     private List<String> indexEditList;
     private List<String> sortedIndexList;
+    private List<Integer> originIndexList;
 
     public void buildIndex() {
         indexEditList = editData.indexEditList;
 
         if(sortedIndexList == null) {
-            sortedIndexList = new ArrayList<>();
+            sortedIndexList = new ArrayList<String>();
+            originIndexList = new ArrayList<Integer>();
         } else {
             sortedIndexList.clear();
+            originIndexList.clear();
         }
 
         indexEditList.stream()
             .sorted()
             .forEach(sortedIndexList::add);
+
+        sortedIndexList.stream()
+            .map(value -> indexEditList.indexOf(value))
+            .forEach(originIndexList::add);
     }//buildIndex()
 
     public void sortByIndex() {
-
+        buildIndex();
+        originIndexList.stream()
+            .forEach(index -> {
+                indexTempList.add(indexEditList.get(index));
+                drinkJpTempList.add(editData.drinkJpEditList.get(index));
+                drinkEnTempList.add(editData.drinkEnEditList.get(index));
+                priceTempList.add(editData.priceEditList.get(index));
+            });
     }//sortByIndex()
-
 
 }//class
