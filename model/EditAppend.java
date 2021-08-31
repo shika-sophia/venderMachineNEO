246a305226533b3f8e9c1a90d654dd1eec29ecde
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-08-31 13:41:12 +0900

    2021-08-31-1400

--- model/EditAppend.java
+++ model/EditAppend.java
@@ -6,7 +6,6 @@ import java.util.Locale;
 import java.util.stream.IntStream;
 
 public class EditAppend extends EditTempLogic {
-    private final int APPEND_SIZE = 4; //appendに必要な追加項目
     private volatile int baseSize;     //append前の drink数
 
     public EditAppend(EditData editData) {
@@ -36,13 +35,13 @@ public class EditAppend extends EditTempLogic {
     public boolean appendOperation(EditMessage editMess) {
         List<String> appList = editData.appendEditList;
 
-        //要素がすべて空なら trueを返し append処理なし
+        //要素がすべて空なら falseを返し append処理なし
         if(!isOrder(appList)) {
-            return true;
+            return false;
         }
 
         //要素が足りない場合 falseを返し 再入力
-        if(appList.size() < APPEND_SIZE) {
+        if(appList.stream().anyMatch(e -> e.equals(""))) {
             editMess.IncorrectAppend();
             return false;
         }
@@ -53,9 +52,10 @@ public class EditAppend extends EditTempLogic {
         String price = appList.get(3);
 
         boolean canIndex = judgeDigit(index);
+        boolean isIndex = judgeStack(index);
         boolean canPrice = judgePrice(price);
 
-        if(!canIndex || !canPrice) {
+        if(!canIndex || !isIndex || !canPrice ) {
             editMess.IncorrectDigit();
             return false;
         }
@@ -68,6 +68,11 @@ public class EditAppend extends EditTempLogic {
 
         return true;
     }//appendOperation()
+
+    private boolean judgeStack(String index) {
+        //defaultIndexListに含まれているなら false
+        return !(defaultIndexList.contains(index));
+    }
 
     //すべての要素が「""」blankなら false
     protected boolean isOrder(List<String> list) {
@@ -88,7 +93,8 @@ public class EditAppend extends EditTempLogic {
         String[] drinkJpDemoAry = {"あ","い","う","え","お"};
         String[] drinkEnDemoAry = {"A","B","C","D","E"};
         String[] priceDemoAry = {"100","110","120","130","140"};
-        String[] appendDemoAry = {"50","か","F","150"};
+        //String[] appendDemoAry = {"50","か","F","150"};
+        String[] appendDemoAry = {"10","か","F","150"};
         //String[] appendDemoAry = {"","か","F","150"};
         //String[] appendDemoAry = {"","","",""};
         String[] deleteDemoAry = {"0"};
@@ -101,10 +107,10 @@ public class EditAppend extends EditTempLogic {
         System.out.println("appendEditList: " + editData.appendEditList);
         System.out.println("appendEditList.size(): " + editData.appendEditList.size());
 
-        System.out.println("indexTempList: " + editTemp.indexTempList);
-        System.out.println("drinkJpTempList: " + editTemp.drinkJpTempList);
-        System.out.println("drinkEnTempList: " + editTemp.drinkEnTempList);
-        System.out.println("priceTempList: " + editTemp.priceTempList);
+        System.out.println("indexTempList: " + editTemp.append.indexTempList);
+        System.out.println("drinkJpTempList: " + editTemp.append.drinkJpTempList);
+        System.out.println("drinkEnTempList: " + editTemp.append.drinkEnTempList);
+        System.out.println("priceTempList: " + editTemp.append.priceTempList);
         System.out.println("editMsg: " + editMess.getEditMsg());
     }//main()
 
@@ -114,10 +120,19 @@ public class EditAppend extends EditTempLogic {
 canAppend: true
 appendEditList: [50, か, F, 150]
 appendEditList.size(): 4
+
+System.out.println("indexTempList: " + editTemp.indexTempList);
 indexTempList: [0, 25, 20, 30, 40]
 drinkJpTempList: [あ, い, う, え, お]
 drinkEnTempList: [A, B, C, D, E]
 priceTempList: [100, 110, 120, 130, 140]
+editMsg: null
+
+System.out.println("indexTempList: " + editTemp.append.indexTempList);
+indexTempList: [0, 25, 20, 30, 40, 50]
+drinkJpTempList: [あ, い, う, え, お, か]
+drinkEnTempList: [A, B, C, D, E, F]
+priceTempList: [100, 110, 120, 130, 140, 150]
 editMsg: null
 
 【考察】 TempListに追加されていない。
@@ -126,4 +141,41 @@ main()内の editTemp.setValue()を削除すると
 
 appendOperation()内の super.setValue()をしないと
 NullPointerExceptionとなる。
+
+editTemp.append.indexTempListで呼び出すと追加されている。
+これは元の EditTempLogicのインスタンスと
+EditAppendの superとしてのEditTempLogicが別々に存在していることを示している。
+
+◆要素空白でも size 4問題
+条件式: if(appList.size() < APPEND_SIZE)
+    appendEditList: [, か, F, 150]
+    appendEditList.size(): 4
+    indexTempList: [0, 25, 20, 30, 40, ]
+
+canAppend -> trueとなり追加されている
+
+条件式: if(appList.stream().anyMatch(e -> e.equals("")))に変更
+canAppend: false
+appendEditList: [, か, F, 150]
+appendEditList.size(): 4
+indexTempList: null
+drinkJpTempList: null
+drinkEnTempList: null
+priceTempList: null
+editMsg: You should input 'Append' all columns.
+
+◆全ての要素が空白
+canAppend: true
+appendEditList: [, , , ]
+appendEditList.size(): 4
+indexTempList: null
+
+canAppend -> true は if(appList.size() < APPEND_SIZE)で returnされたもの
+それでも TempListが nullになるのは おかしい。
+
+◆要素が重複
+String[] appendDemoAry = {"10","か","F","150"};
+canAppend: false
+indexTempList: null
+editMsg: Incorrect as digit.
 */
